1e4f7e4568ed562fe92a6179893dc2f3ff887157
aman <amanraj1172004@gmail.com>
2025-02-01 18:25:04 +0530

    route adding

--- frontend/src/Components/HelpGuest.jsx
+++ frontend/src/Components/HelpGuest.jsx
@@ -62,15 +62,15 @@ const HelpGuest = () => {
           <Link to="/help/all-topic" className="text-gray-500">Browse all topics &gt;</Link>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
-          {[{ img: stay, text: "Finding a stay that's right for you" },
-            { img: cancel, text: "Retired article 3122: How cancellations work" },
-            { img: setupaccount, text: "Setting up your account" },
-            { img: hostel, text: "HostelCover for guests" }]
-            .map(({ img, text }, index) => (
-              <div key={index} className="text-center">
+          {[{ img: stay, text: "Finding a stay that's right for you", link:"#" },
+            { img: cancel, text: "Retired article 3122: How cancellations work",link:"#" },
+            { img: setupaccount, text: "Setting up your account",link:"#" },
+            { img: hostel, text: "HostelCover for guests",link:"#" }]
+            .map(({ img, text,link }, index) => (
+              <Link to={link} key={index} className="text-center cursor-pointer ">
                 <img src={img} alt={text} className="rounded-lg w-full h-40 object-cover mb-2" />
                 <p className="text-xl font-medium">{text}</p>
-              </div>
+              </Link>
             ))}
         </div>
       </div>
@@ -80,14 +80,14 @@ const HelpGuest = () => {
         <h2 className="text-3xl font-bold mb-4">Top articles</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
           {[
-            { title: "Check your reservation status as a guest", desc: "Your reservation status keeps you updated on all kinds of things, such as whether you’re..." },
-            { title: "Deactivating or deleting your account", desc: "We’d hate to see you go, but if you’ve decided to leave Airbnb, you have a couple of options..." },
-            { title: "Verify your phone number", desc: "Let’s connect! Make sure that the right people have your correct phone number. Hosts need..." },
-            { title: "Editing a review you wrote", desc: "To encourage honest and impartial reviews, we..." },
-            { title: "Instant Book", desc: "Want to book a place right away? Try..." },
-          ].map(({ title, desc }, index) => (
+            { title: "Check your reservation status as a guest", desc: "Your reservation status keeps you updated on all kinds of things, such as whether you’re...",link:"#" },
+            { title: "Deactivating or deleting your account", desc: "We’d hate to see you go, but if you’ve decided to leave Airbnb, you have a couple of options...",link:"#" },
+            { title: "Verify your phone number", desc: "Let’s connect! Make sure that the right people have your correct phone number. Hosts need...",link:"#" },
+            { title: "Editing a review you wrote", desc: "To encourage honest and impartial reviews, we...",link:"#" },
+            { title: "Instant Book", desc: "Want to book a place right away? Try...",link:"#" },
+          ].map(({ title, desc,link }, index) => (
             <div key={index} className="w-full md:w-[360px]">
-              <a href="#" className="text-lg underline font-medium text-black">{title}</a>
+              <Link to={link} className="text-lg underline font-medium text-black">{title}</Link>
               <p className="text-gray-500 mb-4">{desc}</p>
               <Divider />
             </div>
@@ -97,21 +97,21 @@ const HelpGuest = () => {
       <div className="bg-black text-white flex flex-col items-center py-10 mt-10">
         <h1 className="text-3xl font-bold mb-6">Explore more</h1>
         <div className="flex flex-wrap justify-center gap-6">
-          {[{ img: policy, title: "Our community policies", desc: "How we build a foundation of trust." },
-            { img: safety, title: "Safety tips and guidelines", desc: "Resources to help travellers stay safe." }]
-            .map(({ img, title, desc }, index) => (
-              <div key={index} className="bg-red-950 rounded-lg overflow-hidden shadow-lg w-80">
+          {[{ img: policy, title: "Our community policies", desc: "How we build a foundation of trust.",link:"#" },
+            { img: safety, title: "Safety tips and guidelines", desc: "Resources to help travellers stay safe.",link:"#" }]
+            .map(({ img, title, desc,link }, index) => (
+              <Link to={link} key={index} className="bg-red-950 cursor-pointer rounded-lg overflow-hidden shadow-lg w-80">
                 <img src={img} alt={title} className="w-full h-40 object-cover" />
                 <div className="p-4">
                   <h2 className="text-lg font-semibold">{title}</h2>
                   <p className="text-sm">{desc}</p>
                 </div>
-              </div>
+              </Link>
             ))}
           <div className=" text-white rounded-lg p-6 shadow-lg text-center md:text-left w-80">
             <h2 className="text-2xl font-semibold mb-4">Need to get in touch?</h2>
             <p className="text-xl mb-4">We'll start with some questions and get <br /> you to the right place.</p>
-            <button className="bg-white text-black w-full font-semibold py-2 px-4 rounded mb-4">Contact us</button>
+            <button className="bg-white cursor-pointer text-black w-full font-semibold py-2 px-4 rounded mb-4">Contact us</button>
             <p className="text-xl">You can also <a href="#" className="underline">give us feedback.</a></p>
           </div>
         </div>
--- frontend/src/Components/HelpHost.jsx
+++ frontend/src/Components/HelpHost.jsx
@@ -7,11 +7,11 @@ import policy from '../assets/policy.png';
 const HelpHost = () => {
   return (
     <div className="p-4">
-      {/* Recommended Section */}
+     
       <div className="container mx-auto max-w-5xl">
         <h1 className="text-2xl font-semibold mb-4">Recommended for you</h1>
         <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2">
-          {/* Identity Verification */}
+         
           <div className="border rounded-lg p-4">
             <div className="flex items-center text-red-600 mb-2">
               <FaExclamationCircle />
@@ -38,7 +38,7 @@ const HelpHost = () => {
           {/* Reservation Details */}
           <div className="border rounded-lg p-4">
             <h2 className="text-lg font-semibold mb-2">Finding your reservation details</h2>
-            <p className="text-gray-600 mb-6">
+            <p className="text-gray-600 mb-26">
               Find all your reservations, news, and to-dos in the Today tab.
             </p>
             <Divider />
@@ -61,24 +61,28 @@ const HelpHost = () => {
             {
               img: "https://images.contentstack.io/v3/assets/bltec2ed8e3c4b1e16d/blta78895c23e1f5ec3/optimizing-your-online-listing-optimized.jpg",
               text: "Optimizing your listing",
+              link:"#"
             },
             {
               img: "https://images.contentstack.io/v3/assets/bltec2ed8e3c4b1e16d/bltc5b5fba2bdc0a264/getting-paid-optimized.jpg",
               text: "Getting paid",
+              link:"#"
             },
             {
               img: "https://images.contentstack.io/v3/assets/bltec2ed8e3c4b1e16d/blt897bd4f509190bff/achieving-your-hosting-goals-optimized.jpg",
               text: "Achieving your hosting goals",
+              link:"#"
             },
             {
               img: "https://images.contentstack.io/v3/assets/bltec2ed8e3c4b1e16d/blt55fbbff760dc003d/changes-cancellations-refunds-optimized.jpg",
               text: "Changes, cancellations, and refunds for Hosts",
+              link:"#"
             },
-          ].map(({ img, text }, index) => (
-            <div key={index} className="text-center">
+          ].map(({ img, text,link }, index) => (
+            <Link to={link} key={index} className="text-center cursor-pointer ">
               <img src={img} alt={text} className="rounded-lg w-full h-40 object-cover mb-2" />
               <p className="text-xl font-medium">{text}</p>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
@@ -88,14 +92,14 @@ const HelpHost = () => {
         <h2 className="text-3xl font-bold mb-4">Top articles</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
           {[
-            { title: "If your guest cancels", desc: "It happens - plans change! If a guest needs to cancel their reservation, we're here to help you..." },
-            { title: "Getting protected through HostelCover for Hosts", desc: "Sometimes accidents happen, which is why there's HostelCover for Hosts. HostelCover for Hosts is..." },
-            { title: "Refund your guest", desc: "It can happen to the best of Hosts: The wifi is a no-show, or the water supply. If something..." },
-            { title: "How a Host cancels a booking", desc: "MyHostel guests look forward to their stays, but we understand there are times when you may..." },
-            { title: "Optimizing your listing", desc: "Check out this essential guide to help you update or fine-tune your listing. Here we are providing..." },
-          ].map(({ title, desc }, index) => (
+            { title: "If your guest cancels", desc: "It happens - plans change! If a guest needs to cancel their reservation, we're here to help you...",link:"#" },
+            { title: "Getting protected through HostelCover for Hosts", desc: "Sometimes accidents happen, which is why there's HostelCover for Hosts. HostelCover for Hosts is...",link:"#" },
+            { title: "Refund your guest", desc: "It can happen to the best of Hosts: The wifi is a no-show, or the water supply. If something...",link:"#" },
+            { title: "How a Host cancels a booking", desc: "MyHostel guests look forward to their stays, but we understand there are times when you may...",link:"#" },
+            { title: "Optimizing your listing", desc: "Check out this essential guide to help you update or fine-tune your listing. Here we are providing...",link:"#" },
+          ].map(({ title, desc,link }, index) => (
             <div key={index} className="w-full md:w-[360px]">
-              <a href="#" className="text-lg underline font-medium text-black">{title}</a>
+              <Link to={link} className="text-lg underline font-medium text-black">{title}</Link>
               <p className="text-gray-500 mb-4">{desc}</p>
               <Divider />
             </div>
@@ -108,21 +112,21 @@ const HelpHost = () => {
         <h1 className="text-3xl font-bold mb-6">Explore more</h1>
         <div className="flex flex-wrap justify-center gap-6">
           {[
-            { img: policy, title: "Our community policies", desc: "How we build a foundation of trust." },
-            { img: "https://images.contentstack.io/v3/assets/bltec2ed8e3c4b1e16d/blte827c6d6cb6360b1/Airbnb-Resources-Web.png", title: "Safety tips and guidelines", desc: "Resources to help travelers stay safe." },
-          ].map(({ img, title, desc }, index) => (
-            <div key={index} className="bg-red-950 rounded-lg overflow-hidden shadow-lg w-80">
+            { img: policy, title: "Our community policies", desc: "How we build a foundation of trust.",link:"#" },
+            { img: "https://images.contentstack.io/v3/assets/bltec2ed8e3c4b1e16d/blte827c6d6cb6360b1/Airbnb-Resources-Web.png", title: "Safety tips and guidelines", desc: "Resources to help travelers stay safe." ,link:"#"},
+          ].map(({ img, title, desc,link }, index) => (
+            <Link to={link} key={index} className="bg-red-950 cursor-pointer rounded-lg overflow-hidden shadow-lg w-80">
               <img src={img} alt={title} className="w-full h-40 object-cover" />
               <div className="p-4">
                 <h2 className="text-lg font-semibold">{title}</h2>
                 <p className="text-sm">{desc}</p>
               </div>
-            </div>
+            </Link>
           ))}
           <div className="text-white rounded-lg p-6 shadow-lg text-center md:text-left w-80">
             <h2 className="text-2xl font-semibold mb-4">Need to get in touch?</h2>
             <p className="text-xl mb-4">We'll start with some questions and get <br /> you to the right place.</p>
-            <button className="bg-white text-black w-full font-semibold py-2 px-4 rounded mb-4">Contact us</button>
+            <button className="bg-white cursor-pointer text-black w-full font-semibold py-2 px-4 rounded mb-4">Contact us</button>
             <p className="text-xl">You can also <a href="#" className="underline">give us feedback.</a></p>
           </div>
         </div>
