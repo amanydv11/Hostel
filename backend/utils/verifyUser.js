e0122d2fc49a5697b2a3049f8d2442f4619e885e
aman <amanraj1172004@gmail.com>
2025-03-20 20:00:30 +0530

    token

--- backend/utils/verifyUser.js
+++ backend/utils/verifyUser.js
@@ -1,15 +1,21 @@
 import jwt from 'jsonwebtoken';
-import {errorHandler} from './error.js'
-export const verifyToken = (req,res,next)=>{
+import { errorHandler } from './error.js';
+
+export const verifyToken = (req, res, next) => {
     const token = req.cookies.access_token;
-    if(!token){
-        return next(errorHandler(401,'Unauthorized'))
+    if (!token) {
+        return next(errorHandler(401, 'Unauthorized - No token provided'));
     }
-    jwt.verify(token,process.env.JWT_SECRET,(err,user)=>{
-        if(err){
-            return next(errorHandler(401,'Unauthorized'));
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) {
+            if (err.name === "TokenExpiredError") {
+                return next(errorHandler(401, 'Token expired. Please log in again.'));
+            }
+            return next(errorHandler(403, 'Unauthorized - Invalid token'));
         }
-        req.user =user;
+
+        req.user = user;
         next();
     });
 };
