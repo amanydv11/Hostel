2c12822bee127e75f7c1422ca5fb0faa5bb3ab8d
aman <amanraj1172004@gmail.com>
2025-03-20 19:34:31 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,7 @@ app.use(cookieParser());
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE","PATCH"],
 }))
 
 const PORT = process.env.PORT || 5000
